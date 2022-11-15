768efbcadbea892f31f0541b8035d3de6a84dcb7
kurbatov <kurbatxx@gmail.com>
2022-11-15 12:46:52 +0600

    add post provider, dialogs, and MainView

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:insta_clone/state/auth/providers/auth_state_provider.dart';
 import 'package:insta_clone/state/auth/providers/is_logged_in_provider.dart';
 import 'package:insta_clone/state/providers/is_loading_provider.dart';
 import 'package:insta_clone/views/components/loading/loading_screen.dart';
 import 'package:insta_clone/views/login/login_view.dart';
+import 'package:insta_clone/views/main/main_view.dart';
 import 'firebase_options.dart';
 
 import 'dart:developer' as devtools show log;
@@ -61,29 +61,6 @@ class App extends StatelessWidget {
           } else {
             return const LoginView();
           }
-        },
-      ),
-    );
-  }
-}
-
-class MainView extends StatelessWidget {
-  const MainView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main View'),
-      ),
-      body: Consumer(
-        builder: (context, ref, child) {
-          return TextButton(
-            onPressed: () async {
-              await ref.read(authStateProvider.notifier).logout();
-            },
-            child: const Text('Logout'),
-          );
         },
       ),
     );
