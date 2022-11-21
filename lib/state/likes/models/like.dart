34a518f34a799accad756736af4a7c5d6f319ba3
kurbatov <kurbatxx@gmail.com>
2022-11-21 17:47:42 +0600

    like_dislike provider

---
+++ lib/state/likes/models/like.dart
@@ -0,0 +1,20 @@
+import 'dart:collection' show MapView;
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:insta_clone/state/comments/typedefs/post_id.dart';
+import 'package:insta_clone/state/constant/firebase_field_name.dart';
+import 'package:insta_clone/state/posts/typedefs/user_id.dart';
+
+@immutable
+class Like extends MapView<String, String> {
+  Like({
+    required PostId postId,
+    required UserId likedBy,
+    required DateTime date,
+  }) : super(
+          {
+            FirebaseFieldName.postId: postId,
+            FirebaseFieldName.userId: likedBy,
+            FirebaseFieldName.date: date.toIso8601String(),
+          },
+        );
+}
---
+++ lib/state/likes/models/like_dislike_request.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:insta_clone/state/comments/typedefs/post_id.dart';
+import 'package:insta_clone/state/posts/typedefs/user_id.dart';
+
+@immutable
+class LikeDislikeRequest {
+  final PostId postId;
+  final UserId likedBy;
+
+  const LikeDislikeRequest({
+    required this.postId,
+    required this.likedBy,
+  });
+}
