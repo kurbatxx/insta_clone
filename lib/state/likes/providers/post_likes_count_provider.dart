0c47aec37d2fc93172fc4c3ec61804b24eaeb8b0
kurbatov <kurbatxx@gmail.com>
2022-11-21 16:07:09 +0600

    wip post details view

---
+++ lib/state/likes/providers/post_likes_count_provider.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:insta_clone/state/comments/typedefs/post_id.dart';
+import 'package:insta_clone/state/constant/firebase_collection_name.dart';
+import 'package:insta_clone/state/constant/firebase_field_name.dart';
+
+final postLikesCountProvider = StreamProvider.family.autoDispose<int, PostId>((
+  ref,
+  postId,
+) {
+  final controller = StreamController<int>.broadcast();
+
+  controller.onListen = () {
+    controller.sink.add(0);
+  };
+
+  final sub = FirebaseFirestore.instance
+      .collection(FirebaseCollectionName.likes)
+      .where(
+        FirebaseFieldName.postId,
+        isEqualTo: postId,
+      )
+      .snapshots()
+      .listen(
+    (snapshot) {
+      controller.sink.add(
+        snapshot.docs.length,
+      );
+    },
+  );
+
+  ref.onDispose(() {
+    sub.cancel();
+    controller.close();
+  });
+
+  return controller.stream;
+});
---
+++ lib/views/components/post/post_diplay_name_and_message_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:insta_clone/state/posts/models/post.dart';
+import 'package:insta_clone/state/user_info/models/user_info_model.dart';
+import 'package:insta_clone/state/user_info/providers/user_info_model_provider.dart';
+import 'package:insta_clone/views/components/animations/small_error_animation_view.dart';
+import 'package:insta_clone/views/components/rich_two_parts_component.dart';
+
+class PostDisplayNameAndMessageView extends ConsumerWidget {
+  final Post post;
+
+  const PostDisplayNameAndMessageView({
+    super.key,
+    required this.post,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userInfoModel = ref.watch(
+      userInfoModelProvider(
+        post.userId,
+      ),
+    );
+    return userInfoModel.when(
+      data: (userInfoModel) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RichTwoPartsText(
+            leftPart: userInfoModel.displayName,
+            rightPart: post.message,
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return const SmallErrorAnimationView();
+      },
+      loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+}
